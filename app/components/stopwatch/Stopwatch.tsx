afcac0082f7bb091566af7f5f10ee1292d285332
Paola Patiño <paolapatino@Mac-mini.home>
2023-07-14 11:24:08 +0200

    WIP: Stopwatch styling, blur shadow

--- app/components/stopwatch/Stopwatch.tsx
+++ app/components/stopwatch/Stopwatch.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { set } from "mongoose";
+"use client";
 import { useEffect, useState } from "react";
 import Button from "../Button";
 
@@ -10,10 +9,10 @@ const Stopwatch = () => {
   useEffect(() => {
     let interval: any;
     if (isRunning) {
-      interval = setInterval(() => setTime(time + 1), 10)
+      interval = setInterval(() => setTime(time + 1), 10);
     }
-    return () => clearInterval(interval)
-  }, [time, isRunning])
+    return () => clearInterval(interval);
+  }, [time, isRunning]);
 
   const hours = Math.floor(time / 360000);
   const minutes = Math.floor((time % 360000) / 6000);
@@ -22,43 +21,58 @@ const Stopwatch = () => {
 
   const startStop = () => {
     setIsRunning(!isRunning);
-  }
+  };
 
   const reset = () => {
     setTime(0);
-  }
+  };
 
   return (
     <div className="relative">
-      <div className="absolute mt-24 flex flex-col items-center p-6
+      <div
+        className="absolute mt-24 flex flex-col items-center p-6
         border border-black border-2
         rounded-lg
         text-neutral-900
-
-      ">
-        <div id="time-container"
+      "
+      >
+        <div
+          id="time-container"
           className="
             text-2xl
             text-amber-200 
             bg-black 
             rounded-xl
             p-3 mb-3
-            
           "
         >
-          <span style={{ textShadow: '1px 1px 2px yellow'}}>{hours.toString().padStart(2, '0')}:</span>
-          <span style={{ textShadow: '1px 1px 2px yellow' }}>{minutes.toString().padStart(2, '0')}:</span>
-          <span style={{ textShadow: '1px 1px 2px yellow' }}>{seconds.toString().padStart(2, '0')}:</span>
-          <span style={{ textShadow: '1px 1px 2px yellow' }} className="text-sm">{milliseconds.toString().padStart(2, '0')}</span>
+          <span style={{ textShadow: "3px 3px 15px #ffffcc" }}>
+            {hours.toString().padStart(2, "0")}:
+          </span>
+          <span style={{ textShadow: "3px 3px 15px #ffffcc"  }}>
+            {minutes.toString().padStart(2, "0")}:
+          </span>
+          <span style={{ textShadow: "3px 3px 15px #ffffcc"  }}>
+            {seconds.toString().padStart(2, "0")}:
+          </span>
+          <span
+            style={{ textShadow: "3px 3px 15px #ffffcc"  }}
+            className="text-sm"
+          >
+            {milliseconds.toString().padStart(2, "0")}
+          </span>
         </div>
         <div id="button-container" className="flex w-[200px]">
-          <Button label={isRunning ? 'Pause' : 'Start'} onClick={startStop}
-            color={ !isRunning ? 'bg-emerald-500' : undefined } />
-          <Button label="Reset" onClick={reset} small outline/>
+          <Button
+            label={isRunning ? "Pause" : "Start"}
+            onClick={startStop}
+            color={!isRunning ? "bg-emerald-500" : undefined}
+          />
+          <Button label="Reset" onClick={reset} small outline />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Stopwatch;
